08d569f68cfffaec35b7d36c67ace9a0eead8027
Vladislav15-maker <vladigra121@email.com>
2025-06-10 22:48:57 +0500

    Further simplify next.config.js for Vercel troubleshooting

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: false, // Изменено на false
-  },
-  eslint: {
-    ignoreDuringBuilds: false, // Изменено на false
-  },
   images: {
     remotePatterns: [
       {
